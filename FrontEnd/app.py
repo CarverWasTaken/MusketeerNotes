6ec4e5c2283246be1a46cbd51dc9cf25d8c36ce5
Carver Simkins <csimkin1@uncc.edu>
2020-11-05 18:55:56 -0500

    Added login page to the front end

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -1,10 +1,24 @@
-from flask import render_template
+from flask import render_template, redirect
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/main")
-def hello():
+@app.route("/")
+def main():
+  return redirect('/login')
+
+@app.route("/login")
+def login():
+  return render_template('login.html')
+
+@app.route("/register")
+def register():
+  return render_template('register.html')
+
+@app.route("/notes")
+def notes():
   return render_template('MainPage.html')
+
+
 
 if __name__ == "__main__":
   app.run()
