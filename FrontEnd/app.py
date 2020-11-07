0b314780af8803bd7af638e84d7cb7d25d880d8b
Carver Simkins <csimkin1@uncc.edu>
2020-11-07 13:51:55 -0500

    Added the ability to add a note to your list

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -3,6 +3,15 @@ from flask import Flask
 from flask import request
 from flask import url_for
 app = Flask(__name__)
+_notes = {
+        0: {'title': 'First Note', 'text': 'This is my first note'},
+        1: {'title': 'Second Note', 'text': 'This is my second note'},
+        2: {'title': 'Third Note', 'text': 'This is my third note'}
+    }
+
+def getNotes():
+   
+   return notes
 
 @app.route("/")
 def main():
@@ -28,7 +37,16 @@ def register():
 
 @app.route("/notes")
 def notes():
-      return render_template('MainPage.html')
+      # _notes = getNotes()
+      return render_template('MainPage.html', notes = _notes)
+
+@app.route("/addNote", methods=['POST'])
+def addNote():
+      if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
+        _notes[len(_notes)+1] = {'title': title, 'text': text}
+        return redirect('/notes')
 
 
 
