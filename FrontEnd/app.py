e68ab79a4bc5b8b719c5085fd34aac5723f0876b
Carver Simkins <csimkin1@uncc.edu>
2020-11-23 19:22:46 -0500

    Added registration

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask import request
 from flask import url_for
 app = Flask(__name__)
-_notes = {
+yourNotes = {
         0: {'title': 'First Note', 'text': 'This is my first note'},
         1: {'title': 'Second Note', 'text': 'This is my second note'},
         2: {'title': 'Third Note', 'text': 'This is my third note'}
@@ -31,21 +31,32 @@ def login():
   else:
       return render_template('login.html')
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-  return render_template('register.html')
+  if request.method == 'POST':
+      username = request.form['Username']
+      password = request.form['Password']
+      confirmPassword = request.form['Confirm']
+
+      #fake login functionality
+      if password == confirmPassword:
+        return redirect('/notes')
+      else:
+        return redirect('/register')
+  else:
+      return render_template('register.html')
 
 @app.route("/notes")
 def notes():
       # _notes = getNotes()
-      return render_template('MainPage.html', notes = _notes)
+      return render_template('MainPage.html', notes = yourNotes)
 
 @app.route("/addNote", methods=['POST'])
 def addNote():
       if request.method == 'POST':
         title = request.form['title']
         text = request.form['text']
-        _notes[len(_notes)+1] = {'title': title, 'text': text}
+        yourNotes[len(yourNotes)+1] = {'title': title, 'text': text}
         return redirect('/notes')
 
 
