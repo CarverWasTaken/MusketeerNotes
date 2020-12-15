330af162456e50c97dced5cc917b983c158cd5da
Carver Simkins <csimkin1@uncc.edu>
2020-12-15 11:53:10 -0500

    Fixed logging in and making comments

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -6,8 +6,8 @@ from flask.globals import session
 from database import db
 from models import Note as Note
 from models import User as User
-from forms import RegisterForm
-from forms import LoginForm
+from models import Comment as Comment
+from forms import RegisterForm, LoginForm, CommentForm
 import bcrypt
 from flask import session
 
@@ -28,22 +28,47 @@ with app.app_context():
 
 # Homepage route
 @app.route("/")
-def main():
+@app.route("/index")
+def index():
     return redirect('/login')
 
 
 # Login page route
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegisterForm()
+    # validate_on_submit only validates using POST
+    if form.validate_on_submit():
+        # form validation included a criteria to check the username does not exist
+        # we can know we are safe to add the user to the database
+        password_hash = bcrypt.hashpw(
+            request.form['password'].encode('utf-8'), bcrypt.gensalt())
+        first_name = request.form['firstname']
+        last_name = request.form['lastname']
+        new_record = User(first_name, last_name, request.form['email'], password_hash)
+        db.session.add(new_record)
+        db.session.commit()
+        # save the user's name to the session
+        session['user'] = first_name
+        # get the id of the newly created database record
+        the_user = db.session.query(User).filter_by(email=request.form['email']).one()
+        # save the user's id to the session
+        session['user_id'] = the_user.id
+
+        return redirect(url_for('notes'))
+    return render_template('register.html', form=form)
+
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     login_form = LoginForm()
     # validate_on_submit only validates using POST
     if login_form.validate_on_submit():
         # we know user exists. We can use one()
-        the_user = db.session.query(User).filter_by(name=request.form['name']).first()
+        the_user = db.session.query(User).filter_by(email=request.form['email']).one()
         # user exists check password entered matches stored password
         if bcrypt.checkpw(request.form['password'].encode('utf-8'), the_user.password):
             # password match add user info to session
-            session['user'] = the_user.name
+            session['user'] = the_user.first_name
             session['user_id'] = the_user.id
             # render view
             return redirect(url_for('notes'))
@@ -55,39 +80,13 @@ def login():
     else:
         # form did not validate or GET request
         return render_template("login.html", form=login_form)
-
-# Register page route -- also contains password authentication
-
 @app.route('/logout')
 def logout():
     # check if a user is saved in session
     if session.get('user'):
         session.clear()
 
-    return redirect(url_for('notes'))
-
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    form = RegisterForm()
-    # validate_on_submit only validates using POST
-    if form.validate_on_submit():
-        # form validation included a criteria to check the username does not exist
-        # we can know we are safe to add the user to the database
-        name = request.form['name']
-        password_hash = bcrypt.hashpw(
-        request.form['password'].encode('utf-8'), bcrypt.gensalt())
-        new_record = User(request.form['name'], password_hash)
-        db.session.add(new_record)
-        db.session.commit()
-        # save the user's name to the session
-        session['user'] = name
-        # get the id of the newly created database record
-        user = db.session.query(User).filter_by(name=request.form['name']).first()
-        # save the user's id to the session
-        session['user_id'] = user.id
-
-        return redirect(url_for('notes'))
-    return render_template('register.html', form=form)
+    return redirect(url_for('login'))
 
 
 # Access to list of notes
@@ -97,16 +96,20 @@ def notes():
     if session.get('user'):
         my_note = db.session.query(Note).filter_by(user_id=session['user_id']).all()
         return render_template('MainPage.html', notes=my_note,user=session['user'])
-    return render_template('MainPage.html')
+    return redirect(url_for('login'))
 
 # Access to indiviual notes with a unique note id
 
 
 @app.route('/note/<note_id>')
 def viewNote(note_id):
-    a_user = db.session.query(User).filter_by(name=User.name)
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
-    return render_template('viewNote.html', note=my_note, user=a_user)
+    if session.get('user'):
+        my_note = db.session.query(Note).filter_by(id=note_id, user_id=session['user_id']).one()
+        
+        form = CommentForm()
+        return render_template("viewNote.html", note = my_note, user = session['user'], form = form)
+    else:
+        return redirect(url_for('login'))
 
 
 # User capibility to add notes -- saves notes to database "db"
@@ -152,53 +155,69 @@ def delete_note(note_id):
     return redirect('/notes')
 
 
-# Route to add a comment to an established note
-@app.route('/note/<note_id>/comment', methods=['GET', 'POST'])
+# # Route to add a comment to an established note
+# @app.route('/note/<note_id>/comment', methods=['GET', 'POST'])
+# def new_comment(note_id):
+#     if request.method == 'POST':
+#         text = request.form['text']
+#         from datetime import date
+#         today = date.today()
+#         today = today.strftime('%m-%d-%Y')
+#         new_record = Comment(text, today, note_id)
+#         db.session.add(new_record)
+#         db.session.commit()
+#         return redirect(url_for('notes'))
+#     else:
+#         # Redirects back to view if page crashes
+#         return redirect(url_for('notes'))
+
+
+# # editing an exisiting comment
+# @app.route('/note/edit/<note_id>/comment', methods=['POST'])
+# def edit_comment(parent_id):
+#     if request.method == 'POST':
+#         # get the data from comment
+#         text = request.form['text']
+
+#         comment = db.session.query(Comment).filter_by(id=parent_id).one()
+
+#         # updtae the comment with new text
+#         comment.text = text
+#         # add the text to database
+#         db.session.add(comment)
+#         db.session.commit()
+
+#         return redirect(url_for('viewNote.html'))
+
+
+# # Delete exisiting note
+# @app.route('/note/delete/<note_id>/comment', methods=['POST'])
+# def delete_comment(note_id, parent_id):
+
+#     comment = db.session.query(Comment).filterby(id=parent_id).one()
+
+#     # delete the comment from database
+#     db.session.delete(comment)
+#     db.session.commit()
+
+#     redirect(url_for('viewNote.html'))
+
+@app.route('/notes/<note_id>/comment', methods=['POST'])
 def new_comment(note_id):
-    if request.method == 'POST':
-        text = request.form['text']
-        from datetime import date
-        today = date.today()
-        today = today.strftime('%m-%d-%Y')
-        new_record = Comment(text, today, note_id)
-        db.session.add(new_record)
-        db.session.commit()
-        return redirect(url_for('notes'))
+    if session.get('user'):
+        comment_form = CommentForm()
+        # validate_on_submit only validates using POST
+        if comment_form.validate_on_submit():
+            # get comment data
+            comment_text = request.form['comment']
+            new_record = Comment(comment_text, int(note_id), session['user_id'])
+            db.session.add(new_record)
+            db.session.commit()
+
+        return redirect(url_for('viewNote', note_id=note_id))
+
     else:
-        # Redirects back to view if page crashes
-        return redirect(url_for('notes'))
-
-
-# editing an exisiting comment
-@app.route('/note/edit/<note_id>/comment', methods=['POST'])
-def edit_comment(parent_id):
-    if request.method == 'POST':
-        # get the data from comment
-        text = request.form['text']
-
-        comment = db.session.query(Comment).filter_by(id=parent_id).one()
-
-        # updtae the comment with new text
-        comment.text = text
-        # add the text to database
-        db.session.add(comment)
-        db.session.commit()
-
-        return redirect(url_for('viewNote.html'))
-
-
-# Delete exisiting note
-@app.route('/note/delete/<note_id>/comment', methods=['POST'])
-def delete_comment(note_id, parent_id):
-
-    comment = db.session.query(Comment).filterby(id=parent_id).one()
-
-    # delete the comment from database
-    db.session.delete(comment)
-    db.session.commit()
-
-    redirect(url_for('viewNote.html'))
-
+        return redirect(url_for('login'))
 
     # start the application "app"
 if __name__ == "__main__":
